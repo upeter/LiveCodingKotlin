f2d69c0f090334057697680e7557dadfc2310c39
upeter <peter.urs@gmail.com>
2018-08-31 17:33:22 +0200

    Delegation exercise

---
+++ labs/src/main/kotlin/org/course/lab03/DelegationExercise.kt
@@ -0,0 +1,65 @@
+package org.course.lab03
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KProperty
+
+/**
+ * Delegation Exercises
+ * Open: DelegationExercise and DelegationExerciseTest
+ * Exercise 1: Class Delegation
+ * Take a look at the Company class. It merely wraps a list of Employees. Consequently, all properties and methods need to be accessed through the employee collection, like:
+ * myCompany.employees.size. In addition, all calls to the logger are delegated to the logger field.
+ * Enhance access to the employee collection and logger by means of class delegation, so that we can:
+ * - access the fields and methods of the employee collection directly through the Company instance like: myCompany.size
+ * - access the methods of the logger directly like this.info("hi")
+ *
+ * Exercise 2: Built in Property Delegates
+ * The averageAge field in Company is always initialized, regardless whether it is accessed or not.
+ * Change the initialization logic for averageAge to use the built-in lazy delegate. Don't forget the log statement when the field is initialized.
+ *
+ * Exercise 3: Custom Property Delegates
+ * In this exercise we want to trace the Employee's age field access. First, take a look at the FieldAccessTracer companion object.
+ * It contains a traceGetter and traceSetter method that keeps track of how many times a particular setter or getter is called.
+ * Convert the FieldAccessTracer into a property delegate implementing the methods of the ReadWriteProperty interface. In the getValue(...) method
+ * make use of traceGetter to track field access. In the setValue(...) method make use of traceSetter to track a field assignment.
+ * Finally, apply the FieldAccessTracer delegate on the Employee's age field.
+ * Make all tests succeed.
+ */
+class Company(val employees: List<Employee>) {
+
+    val averageAge:Double
+    init{
+        logger.info("init averageAge")
+        averageAge = employees.map { it.age }.average()
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(Company::class.java)
+    }
+
+}
+class Employee(var name: String, var age: Int) {
+}
+
+
+class FieldAccessTracer {
+
+    companion object {
+
+        val registry = ConcurrentHashMap<String, Int>()
+        fun key(thisRef: Any?, propName: String, operation: String) = "${thisRef?.javaClass?.name ?: "<unknown>"}.$propName.$operation"
+
+        private fun registerPropertyAccess(thisRef: Any?, propName: String, operation: String) {
+            val key = key(thisRef, propName, operation)
+            registry.put(key, registry.getOrDefault(key, 0) + 1)
+        }
+
+        fun traceGetter(thisRef: Any?, name: String): Unit = registerPropertyAccess(thisRef, name, "get")
+
+        fun traceSetter(thisRef: Any?, name: String): Unit = registerPropertyAccess(thisRef, name, "set")
+    }
+}
+
+
---
+++ labs/src/test/kotlin/org/course/lab03/DelegationExerciseTest.kt
@@ -0,0 +1,77 @@
+package org.course.lab03
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.read.ListAppender
+import io.kotlintest.Description
+import io.kotlintest.TestResult
+import io.kotlintest.extensions.TestListener
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import io.kotlintest.specs.WordSpec
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.functions
+import kotlin.reflect.jvm.javaField
+import ch.qos.logback.classic.Logger as LogbackLogger
+
+class DelegationExerciseTest : WordSpec() {
+    val listAppender = ListAppender<ILoggingEvent>()
+
+    override fun listeners(): List<TestListener> = listOf(TimerListener(listAppender))
+
+
+    init {
+
+        "DelegationExercise.ClassDelegation" should {
+            "Exercise 1: Company class should delegate List and Logger class" {
+                val c = Company(emptyList())
+                c::class.functions.any { it.name == "isEmpty" } shouldBe true
+                c::class.functions.any { it.name == "info" }
+            }
+        }
+        "DelegationExercise.PropertyDelegation" should {
+            "Exercise 2: Company should initialize averageAge lazily and log first access with info"  {
+                val c = Company(listOf(Employee("Jack", 42)))
+                val lazyField = c::class.declaredMemberProperties.find { it.javaField?.type == Lazy::class.java }
+                lazyField shouldNotBe null
+                lazyField?.name shouldBe "averageAge"
+                listAppender.list.clear()
+                c.averageAge shouldBe 42.0
+                //second time the field must be initialized, so no info statement is logged
+                c.averageAge shouldBe 42.0
+                listAppender.list.size shouldBe 1
+            }
+            "Exercise 3: Access to Employee field age (getter and setter) should be registered by FieldAccessTracer"  {
+                FieldAccessTracer.registry.clear()
+                val joe = Employee("Joe", 42)
+                joe.age shouldBe 42
+                joe.age = 24
+                joe.age shouldBe 24
+                println(FieldAccessTracer.registry.keys().toList())
+                FieldAccessTracer.registry.keys().toList().size shouldBe 2
+                val getterKey = FieldAccessTracer.key(joe, "age", "get")
+                FieldAccessTracer.registry.get(getterKey) shouldBe 2
+                val setterKey = FieldAccessTracer.key(joe, "age", "set")
+                FieldAccessTracer.registry.get(setterKey) shouldBe 1
+            }
+
+
+        }
+    }
+
+    class TimerListener(val listAppender: ListAppender<ILoggingEvent>) : TestListener {
+        private val logger: LogbackLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as LogbackLogger
+
+        override fun beforeTest(description: Description): Unit {
+            logger.addAppender(listAppender)
+            listAppender.start()
+        }
+
+        override fun afterTest(description: Description, result: TestResult): Unit {
+            listAppender.stop()
+            listAppender.list.clear()
+            logger.detachAppender(listAppender)
+        }
+    }
+}
