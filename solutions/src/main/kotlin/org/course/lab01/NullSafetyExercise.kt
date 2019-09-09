5b2baf8573e078b80ed3208f0a47f80eeebc0258
upeter <peter.urs@gmail.com>
2019-09-09 13:39:29 +0200

    additional solutions

--- solutions/src/main/kotlin/org/course/lab01/NullSafetyExercise.kt
+++ solutions/src/main/kotlin/org/course/lab01/NullSafetyExercise.kt
@@ -3,7 +3,7 @@ package org.course.lab01
 object
 NullableTypesExercise {
 
-    fun confirmBooking(account: Account?, booking: Booking?):String? {
+    fun confirmBooking(account: Account?, booking: Booking?): String? {
         val email = account?.email
         val hotel = booking?.destination?.hotel
         return if (email != null && hotel != null) {
@@ -11,10 +11,27 @@ NullableTypesExercise {
             val amount = booking.amount?.toString() ?: "FREE"
             createMessage(email, amount, address)
         } else null
+
+//        return account?.email?.run outer@ {
+//            return booking?.destination?.hotel?.run {
+//                val address = "${this.name} ${this.stars.orEmpty()}at ${hotel.address}"
+//                val amount = booking.amount?.toString() ?: "FREE"
+//                createMessage(this@outer, amount, address)
+//            }
+//        }
+
+//        return account?.email?.let { email ->
+//            return booking?.destination?.hotel?.let hotel@{ hotel ->
+//                val address = "${hotel.name} ${hotel.stars.orEmpty()}at ${hotel.address}"
+//                val amount = booking.amount?.toString() ?: "FREE"
+//                createMessage(email, amount, address)
+//            }
+//        }
+
     }
 
     private fun createMessage(email: String, amount: String, hotel: String) =
-        "Confirmation for: $email amount: $amount hotel: $hotel"
+            "Confirmation for: $email amount: $amount hotel: $hotel"
 
 }
 
