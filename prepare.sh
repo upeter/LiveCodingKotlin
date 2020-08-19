14ef78768e9ae1194a96e2758b8df6a34a6cc1fe
upeter <peter.urs@gmail.com>
2020-08-19 15:28:31 +0200

    script to create labs/solutions zip

---
+++ prepare.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SOURCE_DIR=labs
+SOURCE_DIR=solutions
+TARGET_DIR=material
+ARTIFACT_NAME="kotlin-training-${SOURCE_DIR}.zip"
+
+function removeDir {
+find $TARGET_DIR -name $1 -type d -exec rm -rf {} \;
+                }  
+
+function removeFiles {
+find $TARGET_DIR -name $1 -exec rm -rf {} \;
+}
+
+#cleanup target
+rm -rf $TARGET_DIR
+mkdir $TARGET_DIR
+rm -f $ARTIFACT_NAME
+
+#basic copy
+cp -r $SOURCE_DIR/* $TARGET_DIR
+
+rm $ARTIFACT_NAME
+
+#remove directories
+removeDir advanced
+removeDir intermediate
+removeDir bin
+removeDir demo
+removeDir samples
+removeDir logs
+removeDir target
+removeDir project/project
+removeDir worksheets
+removeDir misc
+removeFiles "*.iml"
+removeFiles ".classpath"
+removeFiles ".project"
+removeFiles ".DS_Store"
+removeFiles "*.kts"
+
+cd $TARGET_DIR
+zip -r $ARTIFACT_NAME *
+mv $ARTIFACT_NAME ..
+
+
+
